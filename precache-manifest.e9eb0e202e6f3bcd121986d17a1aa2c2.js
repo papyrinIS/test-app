56f576c93f4849b1f3306a6e77c32290c35d8597
papyrinIS <papyrin@sumirea.ru>
2020-09-11 10:18:26 +0300

    Updates

---
+++ precache-manifest.e9eb0e202e6f3bcd121986d17a1aa2c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f25dd8c4b5a27b6d712ae59203f7b1",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "5434ef74a3e965851af9",
+    "url": "/test-app/static/css/main.463c713b.chunk.css"
+  },
+  {
+    "revision": "43113d1d57e9694b9514",
+    "url": "/test-app/static/js/2.3f0f75b4.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/test-app/static/js/2.3f0f75b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5434ef74a3e965851af9",
+    "url": "/test-app/static/js/main.29f75717.chunk.js"
+  },
+  {
+    "revision": "591160877f109c10f8c4",
+    "url": "/test-app/static/js/runtime-main.2be90cb8.js"
+  },
+  {
+    "revision": "f555bdcf7e814b3f8bbcd21c405d5252",
+    "url": "/test-app/static/media/sortArrows.f555bdcf.svg"
+  },
+  {
+    "revision": "22c0b25f37303bddf85ad11433946c5a",
+    "url": "/test-app/static/media/sortUp.22c0b25f.svg"
+  }
+]);
